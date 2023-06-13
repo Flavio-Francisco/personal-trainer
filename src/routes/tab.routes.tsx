459f446c9ae58d25a6f5839e75473d5d4581b8d6
Flavio Francisco <flavyobass151@gmail.com>
2023-06-13 09:55:32 -0300

    colocando icones da tabbar

--- src/routes/tab.routes.tsx
+++ src/routes/tab.routes.tsx
@@ -1,4 +1,5 @@
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialIcons,Entypo  } from '@expo/vector-icons';
 
 import { Home } from '../screens/Home';
 import { Treiner } from '../screens/Treiner';
@@ -7,43 +8,41 @@ import { Status } from '../screens/Status';
 
 
 
-const {Navigator,Screen} = createBottomTabNavigator();
+const { Navigator, Screen } = createBottomTabNavigator();
 
-export function TabRoutes(){
-    return(
+export function TabRoutes() {
+    return (
         <Navigator>
-   
-         <Screen
-        name='Home'
-        component={Home}
-        options={{
-            headerTitle:'Seja Bem Vindo!',
-            headerTitleAlign:'center',
-            
 
-        
-        }}
-        />
-           <Screen
-        name='Treiner'
-        component={Treiner}
-        options={{
-            headerTitle:'Treinos',
-            headerTitleAlign:'center'
-           
-         
-        }}
-        />
             <Screen
-        name='Status'
-        component={Status}
-        options={{
-            headerShown:false,
-            
-          
-        }}
-        />
-      </Navigator>
+                name='Home'
+                component={Home}
+                options={{
+                    headerTitle: 'Seja Bem Vindo!',
+                    headerTitleAlign: 'center',
+                    tabBarIcon:()=><Entypo name="home" size={24} color="black" />
+
+
+                }}
+            />
+            <Screen
+                name='Treinos'
+                component={Treiner}
+                options={{
+                    headerTitleAlign: 'center',
+                    tabBarIcon: () => <MaterialIcons name="fitness-center" size={24} color="black" />
+                }}
+            />
+            <Screen
+                name='Status'
+                component={Status}
+                options={{
+                    headerShown: false,
+                    tabBarIcon: () =>  <Entypo name="area-graph" size={24} color="black" />
+
+                }}
+            />
+        </Navigator>
 
     );
 }
