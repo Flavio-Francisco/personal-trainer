1a701d06913acee4f010cbb3dc2244746762009c
Flavio Francisco <flavyobass151@gmail.com>
2023-06-19 17:42:02 -0300

    inserindo video player

--- mobile/src/components/CardTreiner.tsx
+++ mobile/src/components/CardTreiner.tsx
@@ -1,6 +1,13 @@
-import { View,Text,StyleSheet } from "react-native";
+import { View,Text,StyleSheet, Alert, TouchableOpacity } from "react-native";
 import { TreinerType } from "../@types/treinerType";
 import { Theme } from './../../Themas';
+import YoutubePlayer, { YoutubeIframeRef } from "react-native-youtube-iframe";
+import React, { useCallback, useRef, useState } from "react";
+import jaja from '../assets/jaja.mp4';
+
+import VideoPlayer from 'expo-video-player'
+import { ResizeMode } from "expo-av";
+
 
 interface Type{
     item:TreinerType,
@@ -9,10 +16,59 @@ interface Type{
 
 export function CardTreiner({item}:Type){
 
+    const [playing, setPlaying] = useState(false);
+    const playerRef = useRef<YoutubeIframeRef>(null);
+    const onStateChange = useCallback((state: string) => {
+        if (state === "ended") {
+          setPlaying(false);
+          Alert.alert("video não encontrado!");
+        }
+      }, []);
+    
+      const togglePlaying = useCallback(() => {
+        setPlaying((prev) => !prev);
+      }, []);
 
 
     return(
         <View style={style.conteiner}>
+            <TouchableOpacity
+            style={style.button}
+            onPress={togglePlaying}
+            onPressIn={()=> {
+                playerRef.current?.getCurrentTime().then(
+                  currentTime => console.log({currentTime})
+                );
+      
+                playerRef.current?.getDuration().then(
+                  getDuration => console.log({getDuration})
+                );
+              }}
+            >
+            <YoutubePlayer
+                ref={playerRef}
+                height={280}
+                play={playing}
+                // para alterar o video e só  alterar o id do video
+                videoId={"-aej3kEcNoU"}
+                onChangeState={onStateChange}
+                 />
+                 
+
+            </TouchableOpacity>
+            <VideoPlayer
+             style={style.video}
+                videoProps={{
+                    
+                    shouldPlay: true,
+                    
+                    resizeMode: ResizeMode.COVER,
+                
+                    source: require('../assets/jaja.mp4')
+                    
+                }}
+/>
+           
             <View style={style.conteinertext}>
               <Text style={style.text1}>Grupo: </Text><Text style={style.text2}>{item.id}</Text>
             </View>
@@ -61,6 +117,14 @@ const style = StyleSheet.create({
     text2:{
         fontSize:20,
         
+    },
+    button:{
+        height:180
+        
+    },
+    video:{
+        height:180,
+        width:310,
     }
 
 })
