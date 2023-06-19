608ad0857c61584dd2ae97296b3f87d4113e8d68
Flavio Francisco <flavyobass151@gmail.com>
2023-06-19 19:34:00 -0300

    configurando cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,9 @@
+import cors from "@fastify/cors";
 import fastify from "fastify";
 
 const server = fastify();
 
+server.register(cors,{origin:true})
 
 server.listen({port:3000},(erroe, address)=>{
     if (erroe) {
